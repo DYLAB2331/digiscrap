a342f7152c066889266cab65443eadc7a4e158a1
DYLAB2331 <dylannguyen2331@gmail.com>
2023-09-30 10:32:04 -0700

    Implemented many-to-many relationship between User and Photo

--- scrapbook/models.py
+++ scrapbook/models.py
@@ -4,7 +4,11 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Photo(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    users = models.ManyToManyField(User, through="UserPhotoThroughRelation")
     image = models.ImageField(upload_to="photos/")
     description = models.TextField()
     date = models.DateField()
+
+class UserPhotoThroughRelation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    photo = models.ForeignKey(Photo, on_delete=models.CASCADE)
