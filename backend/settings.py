ebb055d9f98d23f89b8a05c69df2409a6ddd4bac
DYLAB2331 <dylannguyen2331@gmail.com>
2023-09-29 22:58:03 -0700

    Allowed local connection from mobile device

--- backend/settings.py
+++ backend/settings.py
@@ -30,8 +30,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [env("ALLOWED_HOSTS_PHONE"), env("ALLOWED_HOSTS_PC")]
 
 # Application definition
 
